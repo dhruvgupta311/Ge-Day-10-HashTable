ac70e13de7f9d2660c31b9adb03e203ee2c821ae
Dhruv <guptadhruv1908@gmail.com>
2024-12-11 10:27:37 +0530

    uc1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String sentence = "To be or not to be";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        HashTable hashTable = new HashTable(10);
+
+        String[] words = sentence.split(" ");
+        for (String word : words) {
+            word = word.toLowerCase();
+            hashTable.put(word, 1);
         }
+
+
+        System.out.println("Word Frequencies:");
+        hashTable.printHashTable();
     }
 }
